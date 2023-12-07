91f8e708f4a2bfe6f5a06750e70af703f3c07815
Debasishkaity22 <139854092+Debasishkaity22@users.noreply.github.com>
2023-12-07 21:38:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
 function fibonacci(num) {
 // your code here
+	if(num===0){
+    return 0;
+  }
+  else if(num===1){
+    return 0;
+  }
+  else if(num===2){
+    return 1;
+  }
+  else{
+    let fibo=[0,1];
+    for(let i=2;i<num;i++){
+      fibo[i]=fibo[i-1]+fibo[i-2];
+    }
+    return fibo[num-1];
+  }
+  
 }
 
 module.exports = fibonacci;
